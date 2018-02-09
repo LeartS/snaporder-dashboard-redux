ae385bc772089e78fa1afba8e8d09762cb6e0387
Leonardo Donelli <learts92@gmail.com>
2018-02-09 20:35:26 +0100

    Improve party page layout

--- src/components/party.vue
+++ src/components/party.vue
@@ -1,37 +1,40 @@
 <template>
-<v-content>
-<v-container fluid class="pt-0 ma-0 text-xs-center" id="main">
+<v-content class="snapcolor">
+<v-container fluid class="pt-0 ma-0" id="main">
   <div id="party-code-form" v-if="partyId === null">
   <v-form @submit.prevent="submitPartyCode">
       <v-text-field name="code" label="Party Code"/>
   </v-form>
   </div>
   <div v-if="partyId">
-    <div id="party-details mb-1">
+    <div id="party-details" class="my-5 text-xs-center">
       <h1 class="display-3">{{ partyData.name }}</h1>
-      <p>
+      <p class="headline">
         Partecipanti:
         <span v-for="player in partyData.players" :key="player.id">
         {{player.nick}},
       </span>
       </p>
     </div>
-    <div id="current-round mb-1">
+    <div id="current-round" class="mb-5 text-xs-center">
+      <h2>Round {{ partyData.round + 1 }}</h2>
       <h2>{{ partyData.question }}</h2>
       <h3>{{ answersText }}</h3>
     </div>
     <div id="ranking">
-        <div class="xs-12 md-6">
+        <div id="correct">
+          <h3>Bravi</h3>
           <ul>
-            <li v-for="correct in correctPlays">
-              {{correct.player}}: {{correct.score}}
+            <li v-for="correct in correctPlays" :key="correct.player">
+              {{correct.player}}: <strong class="green--text darken-2">{{correct.score}}</strong>
             </li>
           </ul>
         </div>
-        <div class="xs-12 md-6">
+        <div id="wrong">
+          <h3>Meno Bravi</h3>
           <ul>
-            <li v-for="wrong in wrongPlays">
-              {{wrong.player}}: {{wrong.score}}
+            <li v-for="wrong in wrongPlays" :key="wrong.player">
+              {{wrong.player}}: <strong class="red--text darken-2">{{wrong.score}}</strong>
             </li>
           </ul>
         </div>
@@ -55,6 +58,7 @@ baasbox.appcode = '1234567890'
 const emptyPartyData = {
   name: null,
   players: [],
+  round: -1,
   question: null,
   plays: {},
   answers: [],
@@ -66,11 +70,12 @@ const getDataFromPartyModel = (partyDoc) => {
   return  {
     name: partyDoc.name,
     players: partyDoc.players,
+    round: partyDoc.currentRound,
     question: currentQuestion !== undefined
       ? currentQuestion.question
       : PARTY_START_WAIT_MESSAGE,
     answers: currentQuestion !== undefined
-      ? currentQuestion.answers.map(a => a.answer)
+      ? currentQuestion.answers.map(a => a.answer).sort(() => Math.random() - 0.5)
       : [],
     plays: currentQuestion !== undefined
       ? partyDoc.plays[partyDoc.currentRound]
@@ -108,6 +113,7 @@ export default {
             correct: true,
           }
         })
+        .sort((a, b) => a.score < b.score)
       return c
     },
 
@@ -121,6 +127,7 @@ export default {
             correct: false,
           }
         })
+        .sort((a, b) => a.score < b.score)
       return c
     }
 
@@ -165,10 +172,15 @@ export default {
 </script>
 
 <style scoped>
-#main {
+
+.snapcolor {
   background-color: #ffca28;
+}
+
+#main {
   width: 100%;
   height: 100%;
+  font-size: 2rem;
 }
 
 #party-code-form {
@@ -179,12 +191,21 @@ export default {
 
 #ranking {
   display: flex;
+  width: 70%;
+  margin: auto;
   flex-direction: row;
-  justify-content: center;
+  justify-content: space-around;
 }
 
 #ranking > div {
   flex: 0 1 400px;
 }
-</style>
 
+#ranking > div > h3 {
+  text-align: center;
+}
+
+#ranking ul {
+  list-style: none;
+}
+</style>
